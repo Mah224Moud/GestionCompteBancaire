daee5619594c7237197315e9f7c8ef1e6c7c8aa5
Mamoudou DIALLO <mamoudoudiallo2397@gmail.com>
2023-12-05 00:38:11 +0100

    Init project with maven

---
+++ src/main/java/com/gestion/compte/bancaire/App.java
@@ -0,0 +1,11 @@
+package com.gestion.compte.bancaire;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
