6e7dd05f41384442f8dd259a7e68fdf7f77338aa
Mamoudou DIALLO <mamoudoudiallo2397@gmail.com>
2023-12-07 20:25:49 +0100

    Added hashpassword and Utils

---
+++ src/main/java/com/gestion/compte/bancaire/Utils.java
@@ -0,0 +1,28 @@
+package com.gestion.compte.bancaire;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class Utils {
+
+    public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean checkPassword(String password, String hashedPassword) {
+        return BCrypt.checkpw(password, hashedPassword);
+    }
+
+    public static boolean checkEmail(String email) {
+        return email.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+    }
+
+    public static String generateRandomString(int length) {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int index = (int) (chars.length() * Math.random());
+            sb.append(chars.charAt(index));
+        }
+        return sb.toString();
+    }
+}
